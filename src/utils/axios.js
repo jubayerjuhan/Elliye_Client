62e44fc0c4d92ff883be40a35af91b6c82d65e01
jubayerjuhan <davidjuhan23@gmail.com>
2021-12-06 15:13:21 +0600

    Login added

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 export const authaxios = axios.create({
-  baseURL: 'http://localhost:4000/api/v1',
+  // baseURL: 'http://localhost:4000/api/v1',
+  baseURL: 'https://rocky-eyrie-75260.herokuapp.com/api/v1',
+
   headers: {
     authorization: `Bearer ${localStorage.token ? localStorage.token : ''}`
   }
 });
 export const instance = axios.create({
-  baseURL: 'http://localhost:4000/api/v1',
+  // baseURL: 'http://localhost:4000/api/v1',
+  baseURL: 'https://rocky-eyrie-75260.herokuapp.com/api/v1',
+
 });
 
